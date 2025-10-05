9f336fd4023b04171ffba0b8c5304fd94c8a8adf
JimmyMg <83593009+jimmy107041131@users.noreply.github.com>
2025-10-05 20:52:09 +0800

    Add 2D/3D player controllers, scenes, and assets
    add basic platformer controller (3D and 2D)
    add input key
    add player animation template

---
+++ Scripts/PlayerController.gd
@@ -0,0 +1,141 @@
+extends CharacterBody2D
+
+@export_category("Necesary Child Nodes")
+@export var animated_sprite: AnimatedSprite2D
+@export var collider: CollisionShape2D
+@export var data: Resource
+
+enum State {
+	IDLE,
+	RUN,
+	JUMP,
+	ATTACK,
+	DEAD
+}
+
+#Movement variables
+var move_direction: Vector2 = Vector2.ZERO
+var facing_right: bool = true
+var external_velocity: Vector2 = Vector2.ZERO
+var coyoteActive = false
+var jumpWasPressed = false
+
+#State
+var current_state = State.IDLE
+
+func _ready() -> void:
+	data._init_data()
+
+func _process(delta):
+	_input_handler()
+
+func _physics_process(delta: float) -> void:
+
+	#Move Input
+	move_direction = Input.get_vector("left", "right", "down", "up")
+	if move_direction != Vector2.ZERO:
+		move_direction = move_direction.normalized()
+		facing_right = move_direction.x > 0
+	else:
+		move_direction = Vector2.ZERO
+
+	_ground_check()
+	_move(delta)
+	_state_machine()
+	_animate()
+
+func _input_handler():
+	#Jump
+	if Input.is_action_just_pressed("jump"):
+		jumpWasPressed = true
+		_bufferJump()
+
+func _ground_check():
+	if is_on_floor():
+		coyoteActive = true
+		if jumpWasPressed:
+			_jump()
+	elif coyoteActive:
+		_coyoteTime()
+
+func _move(delta: float) -> void:
+	velocity = _calculate_velocity(velocity, delta)
+	move_and_slide()
+
+
+func _calculate_velocity(vel: Vector2, delta: float) -> Vector2:
+	vel.x = _calculate_horizontal_velocity(vel.x, delta)
+	vel.y = _calculate_vertical_velocity(vel.y, delta)
+	return vel
+
+func _calculate_horizontal_velocity(velX: float, delta: float) -> float:
+	velX = lerp(velX, move_direction.x * data.maxSpeed, data.acceleration * delta)
+
+	if(external_velocity.x != 0):
+		velX = external_velocity.x
+		external_velocity.x = 0
+
+	return velX
+
+func _calculate_vertical_velocity(velY: float, delta: float) -> float:
+	var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") * data.gravityScale
+	if not is_on_floor():
+		gravity *= data.descendingGravityFactor if velY > 0 else 1
+		velY += gravity * delta
+		velY = min(velY, data.terminalVelocity)
+	else:
+		velY = 0
+
+	if(external_velocity.y != 0):
+		velY = external_velocity.y
+		external_velocity.y = 0
+
+	return velY
+
+func _state_machine():
+	match current_state:
+		State.IDLE:
+			if move_direction.x != 0:
+				current_state = State.RUN
+		State.RUN:
+			if move_direction.x == 0:
+				current_state = State.IDLE
+		State.JUMP:
+			if is_on_floor():
+				current_state = State.IDLE
+		State.ATTACK:
+			pass
+		State.DEAD:
+			pass
+
+func _animate():
+	match current_state:
+		State.IDLE:
+			animated_sprite.animation = "idle"
+		State.RUN:
+			animated_sprite.animation = "walk"
+		State.JUMP:
+			animated_sprite.animation = "jump"
+		State.ATTACK:
+			animated_sprite.animation = "attack"
+		State.DEAD:
+			animated_sprite.animation = "dead"
+
+	animated_sprite.flip_h = not facing_right
+	animated_sprite.play()
+
+func _jump():
+	if is_on_floor() or coyoteActive:
+		external_velocity.y = -data.jumpMagnitude
+		coyoteActive = false
+		jumpWasPressed = false
+		current_state = State.JUMP
+
+
+func _coyoteTime():
+	await get_tree().create_timer(data.coyoteTime).timeout
+	coyoteActive = false
+
+func _bufferJump():
+	await get_tree().create_timer(data.jumpBuffering).timeout
+	jumpWasPressed = false
---
+++ Scripts/PlayerData.gd
@@ -0,0 +1,44 @@
+extends Resource
+class_name PlayerData
+
+#INFO HORIZONTAL MOVEMENT 
+@export_category("L/R Movement")
+##The max speed your player will move
+@export_range(0, 500) var maxSpeed: float = 200.0
+##How fast your player will reach max speed from rest (in seconds)
+@export_range(0, 4) var timeToReachMaxSpeed: float = 0.2
+##How fast your player will reach zero speed from max speed (in seconds)
+@export_range(0, 4) var timeToReachZeroSpeed: float = 0.2
+##If true, player will instantly move and switch directions. Overrides the "timeToReach" variables, setting them to 0.
+@export var directionalSnap: bool = false
+##If enabled, the default movement speed will by 1/2 of the maxSpeed and the player must hold a "run" button to accelerate to max speed. Assign "run" (case sensitive) in the project input settings.
+@export var runningModifier: bool = false
+
+var acceleration: float
+var deceleration: float
+
+#INFO JUMPING 
+@export_category("Jumping and Gravity")
+##The peak height of your player's jump
+@export_range(0, 50) var jumpHeight: float = 2.0
+##How many jumps your character can do before needing to touch the ground again. Giving more than 1 jump disables jump buffering and coyote time.
+# @export_range(0, 4) var jumps: int = 1
+##The strength at which your character will be pulled to the ground.
+@export_range(0, 100) var gravityScale: float = 20.0
+##The fastest your player can fall
+@export_range(0, 1000) var terminalVelocity: float = 500.0
+##Your player will move this amount faster when falling providing a less floaty jump curve.
+@export_range(0.5, 3) var descendingGravityFactor: float = 1.3
+##Enabling this toggle makes it so that when the player releases the jump key while still ascending, their vertical velocity will cut in half, providing variable jump height.
+@export var shortHopAkaVariableJumpHeight: bool = true
+##How much extra time (in seconds) your player will be given to jump after falling off an edge. This is set to 0.2 seconds by default.
+@export_range(0, 0.5) var coyoteTime: float = 0.2
+##The window of time (in seconds) that your player can press the jump button before hitting the ground and still have their input registered as a jump. This is set to 0.2 seconds by default.
+@export_range(0, 0.5) var jumpBuffering: float = 0.2
+
+var jumpMagnitude: float = 500.0
+
+func _init_data():
+	acceleration = maxSpeed / timeToReachMaxSpeed
+	deceleration = -maxSpeed / timeToReachZeroSpeed
+	jumpMagnitude = (10.0 * jumpHeight) * gravityScale
