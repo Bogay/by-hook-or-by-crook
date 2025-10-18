654e1dc9f9544b7041645392dcefdca0c21860a1
bogay <pojay11523@gmail.com>
2025-10-19 01:35:07 +0800

    feat: impl take damage for jellyfish

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export_category("Necesary Child Nodes")
 @export var animated_sprite: AnimatedSprite2D
 @export var collider: CollisionShape2D
+@export var attack_area: Area2D
 @export var data: Resource
 
 # HP System
@@ -26,19 +27,26 @@ var external_velocity: Vector2 = Vector2.ZERO
 var coyoteActive = false
 var jumpWasPressed = false
 
+var can_attack: bool = true
+
 #State
 var current_state = State.IDLE
 
 func _ready() -> void:
 	data._init_data()
 	animated_sprite.animation_finished.connect(self._on_animation_finished)
+	attack_area.body_entered.connect(_on_attack_area_body_entered)
+
+func _on_attack_area_body_entered(body):
+	if body.has_method("take_damage") == false:
+		return
+	body.take_damage(data.attack_damage)
 
 func _on_animation_finished():
 	if animated_sprite.animation == "attack2":
+		attack_area.get_node("CollisionShape2D").disabled = true
 		if is_on_floor():
 			current_state = State.IDLE
-		else:
-			current_state = State.JUMP
 
 func _process(_delta):
 	_input_handler()
@@ -48,7 +56,8 @@ func _physics_process(delta: float) -> void:
 	move_direction = Input.get_vector("left", "right", "down", "up")
 	if move_direction != Vector2.ZERO:
 		move_direction = move_direction.normalized()
-		facing_right = move_direction.x > 0
+		if current_state != State.ATTACK:
+			facing_right = move_direction.x > 0
 	else:
 		move_direction = Vector2.ZERO
 
@@ -65,7 +74,9 @@ func _input_handler():
 		jumpWasPressed = true
 		_bufferJump()
 	
-	if Input.is_action_just_pressed("main_attack"):
+	if Input.is_action_just_pressed("main_attack") and can_attack:
+		var mouse_pos = get_global_mouse_position()
+		facing_right = mouse_pos.x > global_position.x
 		current_state = State.ATTACK
 
 func _ground_check():
@@ -125,7 +136,7 @@ func _state_machine():
 			if is_on_floor() and velocity.y == 0:
 				current_state = State.IDLE
 		State.ATTACK:
-			pass
+			_start_attack()
 		State.DEAD:
 			pass
 
@@ -144,6 +155,12 @@ func _animate():
 
 	animated_sprite.flip_h = not facing_right
 	animated_sprite.play()
+
+func _start_attack():
+	can_attack = false
+	attack_area.get_node("CollisionShape2D").disabled = false
+	await get_tree().create_timer(data.attack_cooldown).timeout
+	can_attack = true
 
 func _jump():
 	if (is_on_floor() or coyoteActive) and current_state != State.ATTACK:
